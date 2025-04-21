4333ef24c7cfbaa52f96394f7c9141c49ff2bc4d
NatiTek <natnaelteklu200@gmail.com>
2025-04-22 00:15:03 +0300

    fixed multiple routers error vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import { BrowserRouter as  Routes, Route , BrowserRouter} from 'react-router-dom';
-import Checkout from './pages/Checkout';
-import About from './pages/About';
-import Login from './pages/Login';
-import Custom from './pages/Custom';
+import { BrowserRouter as  BrowserRouter} from 'react-router-dom';
 
 
 
-// import App from './App.tsx'
+import App from './App.tsx'
 import Header from './Header.tsx'
 
 
@@ -19,14 +15,9 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
   <Header itemCount={2} />
-  <Routes>
-    <Route path="/About" element={<About />} />
-    <Route path="/Checkout" element={<Checkout />} />
-    <Route path="/Custom" element={<Custom />} />
-    <Route path="/Login" element={<Login />} />
-  </Routes>
+  <App />
 </BrowserRouter>
  
-    {/* <App /> */}
+  
   </StrictMode>,
 )
