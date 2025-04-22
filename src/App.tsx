b38f198d731a06fe43a9d7a57b42fd90aae2d6db
NatiTek <natnaelteklu200@gmail.com>
2025-04-22 20:14:30 +0300

    yeah

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,1 @@
-import Checkout from './pages/Checkout';
-import About from './pages/About';
-import Login from './pages/Login';
-import Custom from './pages/Custom';
 
-import { BrowserRouter as  Routes, Route } from 'react-router-dom';
-
-
-function App() {
-    return(
-<Routes>
-<Route path="/About" element={<About />} />
-<Route path="/Checkout" element={<Checkout />} />
-<Route path="/Custom" element={<Custom />} />
-<Route path="/Login" element={<Login />} />
-</Routes>
-    )
-}
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -5,17 +5,30 @@ import { BrowserRouter as  BrowserRouter} from 'react-router-dom';
 
 
 
-import App from './App.tsx'
+
 import Header from './Header.tsx'
 
+import Checkout from './pages/Checkout';
+import About from './pages/About';
+import Login from './pages/Login';
+import Custom from './pages/Custom';
 
+import { BrowserRouter as  Routes, Route } from 'react-router-dom';
+
+
+<Routes>
+        <Route path="/Checkout" element={<Checkout />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Custom" element={<Custom />} />
+      </Routes>
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
   <Header itemCount={2} />
-  <App />
+ 
 </BrowserRouter>
  
   
